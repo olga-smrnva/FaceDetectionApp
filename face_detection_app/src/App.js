d983437b5f930b10cd73cf3cf1a485350a85c3c6
Olga Smirnova <smrnva.olga@gmail.com>
2021-12-01 20:45:16 +0200

    reactComponents

--- face_detection_app/src/App.js
+++ face_detection_app/src/App.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImgLinkForm from './components/image_l_f/ImgLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particleOptions = {
+	particles: {
+		number: {
+			value: 70,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+};
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Particles className='particles'
+					params={particleOptions}
+				/>
 				<Navigation />
 				<Logo />
+				<Rank />
 				<ImgLinkForm />
 				{/*
 				<FaceRecognition />} */}
--- face_detection_app/src/components/image_l_f/ImgLinkForm.js
+++ face_detection_app/src/components/image_l_f/ImgLinkForm.js
@@ -9,8 +9,8 @@ const ImgLinkForm = () => {
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' typr='text'/>
-					<button className='w-30 qrow f4 link ph3 pv2 bn dib white bg-light-purple'>Detect</button>
+					<input className='f4 pa2 w-70 center ba b--light-blue' typr='text'/>
+					<button className='w-30 qrow f4 link ph3 pv2 bn grow white bg-light-purple pointer'>Detect</button>
 				</div>
 			</div>
 		</div>
