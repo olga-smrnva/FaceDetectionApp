b6abcd3950f43705f2fbefd4f3d77f655de71214
Olga Smirnova <smrnva.olga@gmail.com>
2021-12-06 18:43:59 +0200

    api

--- face_detection_app/src/App.js
+++ face_detection_app/src/App.js
@@ -7,8 +7,52 @@ import FaceRecognition from './components/face_recognition/FaceRecognition';
 import Particles from "react-tsparticles";
 import ParticlesConfig from './components/particles/ParticlesConfig';
 import './App.css';
+
+const raw = JSON.stringify({
+	"user_app_id": {
+			"user_id": "6wnn6umeuz57",
+			"app_id": "9c5180a13ef1415199b0b8f25b3c672d"
+	  },
+	"inputs": [
+		{
+			"data": {
+				"image": {
+					"url": "https://samples.clarifai.com/metro-north.jpg"
+				}
+			}
+		}
+	]
+});
+  
+const requestOptions = {
+	method: 'POST',
+	headers: {
+		'Accept': 'application/json',
+		'Authorization': 'Key b5dc65dc60c0414daed11280d9e3f2bd'
+	},
+	body: raw
+};
     
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			input: '',
+		};
+	};
+
+	onInputChange = (event) => {
+		console.log(event.target.value);
+	};
+
+	onButtonSubmit = () => {
+		console.log('click');
+		fetch("https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/versions/{MODEL_VERSION_ID}/outputs", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(JSON.parse(result, null, 2).outputs[0].data))
+			.catch(error => console.log('error', error));
+};
+
 	render() {
 		return (
 			<div className="App">
@@ -18,7 +62,10 @@ class App extends Component {
 				<Navigation />
 				<Logo />
 				<Rank />
-				<ImgLinkForm />
+				<ImgLinkForm 
+					onInputChange={this.onInputChange} 
+					onButtonSubmit={this.onButtonSubmit}
+				/>
 				<FaceRecognition />
 			</div>
 		);
--- face_detection_app/src/components/image_l_f/ImgLinkForm.js
+++ face_detection_app/src/components/image_l_f/ImgLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImgLinkForm.css';
 
-const ImgLinkForm = () => {
+const ImgLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
@@ -9,8 +9,12 @@ const ImgLinkForm = () => {
 			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center ba b--light-blue' typr='text'/>
-					<button className='w-30 qrow f4 link ph3 pv2 bn grow white bg-light-purple pointer'>Detect</button>
+					<input className='f4 pa2 w-70 center ba b--light-blue' type='text' 
+					onChange={onInputChange}/>
+					<button 
+						className='w-30 qrow f4 link ph3 pv2 bn grow white bg-light-purple pointer'
+						onClick={onButtonSubmit}
+					>Detect</button>
 				</div>
 			</div>
 		</div>
@@ -18,3 +22,6 @@ const ImgLinkForm = () => {
 }; 
 
 export default ImgLinkForm;
+
+
+   
