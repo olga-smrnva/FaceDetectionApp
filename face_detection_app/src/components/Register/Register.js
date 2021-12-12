c71dfc0ba4e0a749eaac07c1bcec8366b3c6e9d5
Olga Smirnova <smrnva.olga@gmail.com>
2021-12-12 19:58:27 +0200

    minorChanges

--- face_detection_app/src/components/Register/Register.js
+++ face_detection_app/src/components/Register/Register.js
@@ -10,7 +10,7 @@ const Register = ({ onRouteChange }) => {
 				<div className="mt3">
 					<label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
 					<input
-					className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+					className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
 					type="text"
 					name="name"
 					id="name"
@@ -20,7 +20,7 @@ const Register = ({ onRouteChange }) => {
 				<div className="mt3">
 					<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
 					<input
-					className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+					className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
 					type="email"
 					name="email-address"
 					id="email-address"
@@ -30,7 +30,7 @@ const Register = ({ onRouteChange }) => {
 				<div className="mv3">
 					<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
 					<input
-					className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+					className="b pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
 					type="password"
 					name="password"
 					id="password"
